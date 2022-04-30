d8373585b38c0ce02aa3c099db5d4b980248e678
SN-Grotesque <z7z-h@outlook.com>
2022-04-30 16:29:41 +0800

    VPN

--- py/demo1/proxy.py
+++ py/demo1/proxy.py
@@ -19,7 +19,14 @@ def server_init(host, port):
     s.listen(5)
     return s.accept()
 
-
+def hexdump(src :bytes):
+    length, result, digits = 32, [], 2
+    for i in range(0, len(src), length):
+        s = src[i : i+length]
+        hexa = ''.join(["%02X "%x for x in s])
+        text = "".join([chr(x if 0x20 <= x < 0x7F else 0x2E) for x in s])
+        result.append("%08X  %-*s %s" % (i, length * (digits + 1), hexa, text))
+    print('\n'.join(result))
 
 
 
